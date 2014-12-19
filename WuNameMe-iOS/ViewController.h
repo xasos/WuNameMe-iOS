191b2da588d14ce3b0ca032e32183a7e7f4144cf
Niraj Pant <nirajpant7@gmail.com>
2014-12-19 17:07:51 -0600

    Initial Commit

---
+++ WuNameMe-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WuNameMe-iOS
+//
+//  Created by Niraj  on 12/19/14.
+//  Copyright (c) 2014 Niraj Pant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WuNameMe-iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WuNameMe-iOS
+//
+//  Created by Niraj  on 12/19/14.
+//  Copyright (c) 2014 Niraj Pant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
